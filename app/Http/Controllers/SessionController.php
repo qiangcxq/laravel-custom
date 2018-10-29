0d63210629fcefe047ae0372b57bf3a9bcd9972d
chenbo <chenbotome@163.com>
2018-10-29 15:03:24 +0800

    fix

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -12,15 +12,16 @@ namespace App\Http\Controllers;
 use App\Services\UserService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Log;
 
 class SessionController extends Controller
 {
     public function login(Request $request){
         $code = $request->post('code');
         $userInfo = $request->post('user_info');
+        Log::info('测试', $userInfo);
         $userInfo = $userInfo['detail']['userInfo'];
         list($openId, $sessionKey) = $this->getWechatService()->getCode2Session($code);
-//        list($openId, $sessionKey) = ['aa','bb'];
         $userInfo['session_key'] = $sessionKey;
         $userInfo['open_id'] = $openId;
         if (resolve(UserService::class)->ifExistByOpenId($openId)){
