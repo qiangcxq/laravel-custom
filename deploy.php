41b8b0194240962b428defd532e73053eb7c618f
chenbo <chenbotome@163.com>
2018-11-16 17:07:23 +0800

    feature deploy nginx

--- deploy.php
+++ deploy.php
@@ -16,7 +16,7 @@ set('git_tty', false);
 
 // Shared files/dirs between deploys 
 set('shared_files', []);
-set('shared_dirs', ['deploy']);
+set('shared_dirs', ['deploy', 'storage', 'test']);
 
 // Writable dirs by web server 
 set('writable_dirs', ['storage']);
@@ -25,16 +25,17 @@ set('allow_anonymous_stats', false);
 // Hosts
 
 host('114.115.138.227')
+    ->hostname('114.115.138.227')
     ->user('root')
     ->set('branch', 'book')
     ->set('http_user', 'www-data')
     ->set('deploy_path', '/var/www/html/{{application}}');
 
 // Tasks
-task('nginx', function (){
-    run('cp {{deploy_path}}/laravel-custom/deploy/book /etc/nginx/sites-available/book');
-    run('ln -s /etc/nginx/sites-available/book /etc/nginx/sites-enabled/book');
-});
+//ask('adsfa', 'chenbo');
+//if (commandExist('composer')) {
+//    parse('不存在composer');
+//}
 desc('Deploy your project');
 task('deploy', [
     'deploy:info',
@@ -54,6 +55,18 @@ task('deploy', [
 
 task('deploy:info', function (){
     writeln('部署Book');
+});
+
+
+after('success', 'deploy:nginx');
+task('deploy:nginx', function () {
+    $result = askChoice('reload nginx with new config ?', ['yes'=>'y', 'no'=>'n'], 'no', true)[0];
+    if ($result === 'yes') {
+        run('cp {{deploy_path}}/shared/deploy/book /etc/nginx/sites-available/book');
+        run('rm /etc/nginx/sites-enabled/book');
+        run('ln -s /etc/nginx/sites-available/book /etc/nginx/sites-enabled/book');
+        run('service nginx reload');
+    }
 });
 
 // [Optional] If deploy fails automatically unlock.
